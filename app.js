d1bf58f43fe954dcf0f82a0e284ce819e8084b65
Joltay <55696831+Joltay@users.noreply.github.com>
2019-10-23 10:21:16 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 let country = 'Казахстане';
 let age = 18;
 
-let result = Совершенолетие в + ' ' + country + ' ' + наступает в + ' ' + age + ' ' + лет.
+let result = Совершенолетие в + ' ' + country + ' ' + наступает в + ' ' + age + ' ' + лет.;
 
 alert(result);
